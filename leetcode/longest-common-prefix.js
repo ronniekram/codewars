92e30aac93cb80e33a0d5101f96f2490512900bc
Arynn Boniface <arynn.boniface@gmail.com>
2020-11-26 22:16:24 -0500

    needle in the haystack challenge

---
+++ leetcode/longest-common-prefix.js
@@ -0,0 +1,15 @@
+var longestCommonPrefix = function(strs) {
+    let prefix = ""
+    if(strs === null || strs.length === 0) return prefix
+
+    for (let i=0; i < strs[0].length; i++){ 
+        const char = strs[0][i]
+
+        for (let j = 1; j < strs.length; j++){ 
+            if(strs[j][i] !== char) return prefix
+        }
+        prefix = prefix + char
+    }
+
+    return prefix
+}
---
+++ leetcode/needle-haystack.js
@@ -0,0 +1,14 @@
+// Implement strStr().
+
+// Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+// Clarification:
+
+// What should we return when needle is an empty string? This is a great question to ask during an interview.
+
+// For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
+
+var strStr = function(haystack, needle) {
+    if (haystack === "" && needle === "") return 0;
+    return haystack.indexOf(needle);
+  };
