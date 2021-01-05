3249eaec966a127fa07c8e3e4abd2b204857999e
Arynn Boniface <arynn.boniface@gmail.com>
2021-01-05 01:07:26 -0500

    added solution and results for reverse.js

---
+++ leetcode/reverse.js
@@ -0,0 +1,7 @@
+var reverseString = function(s) {
+  return s.reverse();
+};
+
+
+// Runtime: 128 ms, faster than 12.64% of JavaScript online submissions for Reverse String.
+// Memory Usage: 45.7 MB, less than 67.17% of JavaScript online submissions for Reverse String.
