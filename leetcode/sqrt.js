0bc8361bb6c61175ec5c2da9f930caf0d2e1d950
Arynn Boniface <arynn.boniface@gmail.com>
2021-01-04 21:15:46 -0500

    added solution and results for truncated square root

---
+++ leetcode/sqrt.js
@@ -0,0 +1,6 @@
+var mySqrt = function(x) {
+  return Math.trunc(Math.sqrt(x))
+};
+
+// Runtime: 92 ms, faster than 87.20% of JavaScript online submissions for Sqrt(x).
+// Memory Usage: 40 MB, less than 41.06% of JavaScript online submissions for Sqrt(x).
